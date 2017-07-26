516d3ac94e0ce27d66a40097b277ffaad581e9b2
Bex Warner <hiimbex@github.com>
2017-07-25 18:26:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,8 @@
-const standard = require('standard')
+console.log('test!');
 
-module.exports = {
-  async push(event, context) {
-    const push = event.payload
+console.log("testing!!!");
 
-    const compare = await context.github.repos.compareCommits(context.repo({
-      base: push.before,
-      head: push.after
-    }));
+cosnole.log('kk')
 
-    const branch = push.ref.replace('refs/heads/', '')
 
-    return Promise.all(compare.data.files.map(async file => {
-      const content = await context.github.repos.getContent(context.repo({
-        path: file.filename,
-        ref: branch
-      }));
-        
-      const text = Buffer.from(content.data.content, 'base64').toString();
-
-      standard.lintText(text, {cwd: "", fix: true, filename: file.filename}, (err, results) => {
-        console.log(results);
-        return Promise.all(results.results.map(result => {
-          context.github.repos.updateFile(context.repo({
-            path: file.filename,
-            message: `Fix lint errors for ${file.filename}`,
-            content: Buffer.from(result.output).toString('base64'),
-            sha: content.data.sha,
-            branch: branch,
-          }));
-        }));
-      });
-    }));
-  }
-
+   
