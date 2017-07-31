eb49d43f5caffb965b9b5c988d38c8bbb23c201e
Bex Warner <hiimbex@github.com>
2017-07-30 18:09:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ console.log('')
 
 console.log('test')
 async function foo(){console.log("test")}
+
+
+Object.assign(linterItems, {cwd: '', fix: true, filename: file.filename});
+[1, 2, 3].map(n => n **2);
